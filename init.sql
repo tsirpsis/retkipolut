c85c4404597ef4299b21aa3334b616c9ae846c80
Terhi <180004843+tsirpsis@users.noreply.github.com>
2026-05-01 09:54:07 +0300

    Add new category and remove futile code rows

--- init.sql
+++ init.sql
@@ -9,3 +9,10 @@ INSERT INTO classes (title, value) VALUES ('Reitin kunto', 'Erinomainen');
 INSERT INTO classes (title, value) VALUES ('Reitin kunto', 'Pääosin hyväkuntoinen');
 INSERT INTO classes (title, value) VALUES ('Reitin kunto', 'Paikoin huonokuntoinen');
 INSERT INTO classes (title, value) VALUES ('Reitin kunto', 'Vaivoin kuljettavissa');
+
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', '1 - 3 km');
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', '4 - 6 km');
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', '7 - 12 km');
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', '13 - 18 km');
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', '19 - 25 km');
+INSERT INTO classes (title, value) VALUES ('Reitin pituus', 'Yli 25 km');
